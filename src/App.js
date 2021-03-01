f010d9febafb033c1dd5ec266816550601ee65d0
jeel-lap2-win <jeelpdev@gmail.com>
2021-03-01 22:00:27 +0530

    created env file and structured code, added other file process options dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,133 +1,116 @@
-import React,{Component} from 'react';
+import React, { useState } from 'react'
 
-import axios from 'axios';
+import axios from 'axios'
 
-class App extends Component {
-
-	state = {
-
-	// Initially, no file is selected
-	selectedFile: null
-	};
+const App = () => {
+	const [selectedFile, setSelectedFile] = useState(null)
+	const [selectedProcess, setSelectedProcess] = useState("compress")
 	
 	// On file select (from the pop up)
-	onFileChange = event => {
-	
-	// Update the state
-	this.setState({ selectedFile: event.target.files[0] });
-	
-	};
+	const onFileChange = event => {
+		setSelectedFile(event.target.files[0])
+	}
+
+	const onProcessChange = event => {
+		setSelectedProcess(event.target.value)
+	}
 
 	// On file upload (click the upload button)
-	onFileUpload = () => {
-	
-	// Create an object of formData
-	const formData = new FormData();
-	
-	// Update the formData object
-	formData.append(
-		"avatar",
-		this.state.selectedFile,
-		this.state.selectedFile.name
-	);
-	
-	// Details of the uploaded file
-	console.log(" uploading file ,,,");
-	
-	// Request made to the backend api
-	// Send formData object
-	axios.post("http://localhost:4000/compression", formData)
-	.then(res => {
-    console.log(" received success message ,,,", res)
-
-	return axios({
-		method: 'GET',
-		url: `http://localhost:4000/processedfile/${res.data.fileId}`,
-		responseType: 'blob'
-		}).then(res => {
-			var data = res.data
-	
-			console.log('downloaded ,,,')
-	
-		//TODO: to download after conversion, but PDF data is getting blank only blank pages are downloaded or converted from data
-		var saveByteArray = (function () {
-			var a = document.createElement("a");
-			document.body.appendChild(a);
-			a.style = "display: none";
-			return function (data, name) {
-				var blob = new Blob(data,
-									// {type: "octet/stream"}),
-									{type: "application/pdf"}),
-									url = window.URL.createObjectURL(blob);
-				a.href = url;
-				a.download = name;
-				a.click();
-				window.URL.revokeObjectURL(url);
-			};
-		}());
+	const onFileUpload = () => {
+		// Create an object of formData
+		const formData = new FormData()
 		
-		saveByteArray([data], `${this.state.selectedFile.name}.pdf`);
+		// Update the formData object
+		formData.append(
+			"avatar",
+			selectedFile,
+			selectedFile.name
+		)
+	
+		// Request made to the backend api, Send formData object
+		axios.post(`${process.env.REACT_APP_API_URL}/${selectedProcess}`, formData)
+		.then(res => {
+
+			console.log(" received success message ,,,", res)
+
+			// on success of file process, chaining downloading request
+			return axios({
+				method: 'GET',
+				url: `${process.env.REACT_APP_API_URL}/processedfile/${res.data.fileId}`,
+				responseType: 'blob'
+			}).then(res => {
+				var data = res.data
+		
+				console.log('downloaded ,,,')
+		
+				// auto downloads the file for user
+				var saveByteArray = (function () {
+					var a = document.createElement("a")
+					document.body.appendChild(a)
+					a.style = "display: none"
+					return function (data, name) {
+						var blob = new Blob(data, { type: "application/pdf" }), url = window.URL.createObjectURL(blob)
+						a.href = url
+						a.download = name
+						a.click()
+						window.URL.revokeObjectURL(url)
+					}
+				}())
+				
+				saveByteArray([data], `${selectedProcess}-${selectedFile.name}.pdf`)
+			})
+
+  		}).catch(e => {
+			console.log("Error occured!", e)
 		})
-
-  }).catch(e => {
-    console.log("e!!!", e)
-  });
-
-	};
+	}
 	
 	// File content to be displayed after
-	// file upload is complete
-	fileData = () => {
-	
-	if (this.state.selectedFile) {
-		
-		return (
-		<div>
-			<h2>File Details:</h2>
-			
-<p>File Name: {this.state.selectedFile.name}</p>
-
-			
-<p>File Type: {this.state.selectedFile.type}</p>
-
-			
-<p>
-			Last Modified:{" "}
-			{this.state.selectedFile.lastModifiedDate.toDateString()}
-			</p>
-
-		</div>
-		);
-	} else {
-		return (
-		<div>
-			<br />
-			<h4>Choose before Pressing the Upload button</h4>
-		</div>
-		);
+	const fileData = () => {
+		if (selectedFile) {
+			return (
+				<div>
+					<h2>File Details:</h2>
+					<p>File Name: {selectedFile.name}</p>
+					<p>File Type: {selectedFile.type}</p>
+					<p>Last Modified:{" "}
+						{selectedFile.lastModifiedDate.toDateString()}
+					</p>
+				</div>
+			)
+		} else {
+			return (
+			<div>
+				<br />
+				<h4>Choose before Pressing the Upload button</h4>
+			</div>
+			)
+		}
 	}
-	};
-	
-	render() {
 	
 	return (
 		<div>
-			<h1>
-			compression
-			</h1>
-		
+			<label htmlFor="process">Choose what to do:</label>
+			<select name="process" id="process" onChange={onProcessChange} value={selectedProcess}>
+				<option value="compression">Compress PDF</option>
+				<option value="unlock">Unlock PDF</option>
+				<option value="encrypt">Encrypt PDF</option>
+				<option value="decrypt">Decrypt PDF</option>
+				<option value="pageNumber">Add page numbers to PDF</option>
+				<option value="merge">Merge PDF</option>
+				<option value="convert">Convert to PDF</option>
+			</select>
 			<div>
-				<input type="file" onChange={this.onFileChange} />
-				<button onClick={this.onFileUpload}>
-				Upload
+				<input type="file" onChange={onFileChange} />
+				<button
+					onClick={onFileUpload}
+				>
+					Upload
 				</button>
 			</div>
-
-		{this.fileData()}
-
+			{fileData()}
 		</div>
-	);
-	}
+	)
 }
 
-export default App;
+export default App
